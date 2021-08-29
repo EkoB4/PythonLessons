98d4b2712494145ab61bb8944b6d1cc89863ff60
Ekin Bacik <ekinbacik@gmail.com>
2021-08-29 22:26:30 +0300

    New repo for py lessons.

---
+++ learnPy.py
@@ -0,0 +1,32 @@
+#EkoB4
+"""
+part 1 -> Comments
+Comment satiri eklemek icin tekil bir satiri hesaba katiyor isek
+"#" kullaniriz .
+Commentler kodun soluna, assagisina, ustune gelebilir .
+Soluna gelirse tabiki tum satir yorum olur .
+
+part 1.5 -> Multi Line Comments
+az once tekil yorum satirlarini ogrendik, peki coklu satir
+nasil kullanilir? Iki secenek var birisi her satira "#" kullanmak
+ikinci secenek 3 tirnak (" " ") altli ve ustlu olarak kullanilarak 
+olusturulur.
+"""
+
+#Part 2 -> Variables
+"""
+Pythonda herhangi bir degeri belirtmek icin ozel bir komuta gerek yok.
+Ornegin{
+Swift ui :                       Python:
+var a : String = "My String"            a = "My String"
+}
+
+Part 2.5 -> Data Types
+String -> myStr = "3"
+Int -> myNum = 3
+Float -> myNum = 3.0
+Ciktisi:
+"""
+Str = str(3)
+Int = int(3)
+Float = float(3)
